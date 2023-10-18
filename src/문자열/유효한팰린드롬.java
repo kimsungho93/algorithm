747724ef803dc9296487eb1956eeb7ccf983f577
kimsungho <tigertjdgh@gmail.com>
2023-10-18 22:34:21 +0900

    문자열 : 유효한팰린드롬

---
+++ src/문자열/유효한팰린드롬.java
@@ -0,0 +1,41 @@
+package 문자열;
+
+import java.util.Scanner;
+
+/**
+ * 유효한 팰린드롬
+ *
+ * 앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 팰린드롬이라고 합니다.
+ * 문자열이 입력되면 해당 문자열이 팰린드롬이면 "YES", 아니면 “NO"를 출력하는 프로그램을
+ * 작성하세요.
+ * 단 회문을 검사할 때 알파벳만 가지고 회문을 검사하며, 대소문자를 구분하지 않습니다.
+ * 알파벳 이외의 문자들의 무시합니다.
+ * ▣ 입력설명
+ * 첫 줄에 길이 100을 넘지 않는 문자열이 주어집니다.
+ * ▣ 출력설명
+ * 첫 번째 줄에 팰린드롬인지의 결과를 YES 또는 NO로 출력합니다.
+ * ▣ 입력예제 1
+ * found7, time: study; Yduts; emit, 7Dnuof
+ * ▣ 출력예제 1
+ * YES
+ */
+public class 유효한팰린드롬 {
+    public String solution(String str) {
+        String answer = "NO";
+        str = str.toUpperCase().replaceAll("[^A-Z]", "");
+        String reverseStr = new StringBuilder(str).reverse().toString();
+
+        if (str.equals(reverseStr)) {
+            answer = "YES";
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        유효한팰린드롬 유효한팰린드롬 = new 유효한팰린드롬();
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println(유효한팰린드롬.solution(str));
+    }
+}
